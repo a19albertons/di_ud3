bcdfac53836073b1491214a80d39b7758b172733
a19albertons <a19albertons@iessanclemente.net>
2025-11-05 13:01:40 +0100

    Some fixes and bottom

--- MasterRollerDice/app/src/main/java/com/example/masterrollerdice/MainActivity.kt
+++ MasterRollerDice/app/src/main/java/com/example/masterrollerdice/MainActivity.kt
@@ -27,28 +27,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        // Get NavHostFragment by the FragmentContainerView id (NOT the nav_graph id)
-        val navHost = supportFragmentManager
-            .findFragmentById(R.id.fragment_container_view) as NavHostFragment
-        navController = navHost.navController
-
-        // Hook bottom navigation to navController
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_toolbar, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item)
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
